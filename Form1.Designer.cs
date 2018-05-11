de662fd09ba932c02dab8ab1e32a7830ba5ac55f
mieshki <miszek_12@wp.pl>
2018-05-11 19:31:12 +0200

    Completed

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace YoutubeSearchAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.videoSearchBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.videoSearchBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.videoSearchBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.videoSearchBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            this.thumbnailDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.videoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.CausesValidation = false;
+            this.btnSearch.Location = new System.Drawing.Point(418, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 36);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(400, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // videoSearchBindingSource
+            // 
+            this.videoSearchBindingSource.DataSource = typeof(YoutubeSearch.VideoSearch);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.thumbnailDataGridViewImageColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn,
+            this.urlDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.videoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 90;
+            this.dataGridView1.Size = new System.Drawing.Size(481, 329);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // videoSearchBindingSource1
+            // 
+            this.videoSearchBindingSource1.DataSource = typeof(YoutubeSearch.VideoSearch);
+            // 
+            // videoSearchBindingSource2
+            // 
+            this.videoSearchBindingSource2.DataSource = typeof(YoutubeSearch.VideoSearch);
+            // 
+            // videoSearchBindingSource3
+            // 
+            this.videoSearchBindingSource3.DataSource = typeof(YoutubeSearch.VideoSearch);
+            // 
+            // thumbnailDataGridViewImageColumn
+            // 
+            this.thumbnailDataGridViewImageColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.thumbnailDataGridViewImageColumn.DataPropertyName = "Thumbnail";
+            this.thumbnailDataGridViewImageColumn.FillWeight = 400F;
+            this.thumbnailDataGridViewImageColumn.HeaderText = "Thumbnail";
+            this.thumbnailDataGridViewImageColumn.Name = "thumbnailDataGridViewImageColumn";
+            this.thumbnailDataGridViewImageColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "Author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "Author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // urlDataGridViewTextBoxColumn
+            // 
+            this.urlDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.urlDataGridViewTextBoxColumn.DataPropertyName = "Url";
+            this.urlDataGridViewTextBoxColumn.HeaderText = "Url";
+            this.urlDataGridViewTextBoxColumn.Name = "urlDataGridViewTextBoxColumn";
+            // 
+            // videoBindingSource
+            // 
+            this.videoBindingSource.DataSource = typeof(YoutubeSearchAPI.Video);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 386);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Youtube Search API";
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoSearchBindingSource3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.BindingSource videoSearchBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource videoSearchBindingSource3;
+        private System.Windows.Forms.BindingSource videoSearchBindingSource1;
+        private System.Windows.Forms.BindingSource videoSearchBindingSource2;
+        private System.Windows.Forms.BindingSource videoBindingSource;
+        private System.Windows.Forms.DataGridViewImageColumn thumbnailDataGridViewImageColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeSearch;
+using YoutubeSearchAPI;
+
+namespace YoutubeSearchAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            VideoSearch items = new VideoSearch();
+            List<Video> list = new List<Video>();
+
+            foreach(var item in items.SearchQuery(txtSearch.Text, 1))
+            {
+                Video video = new Video();
+                video.Title = item.Title;
+                video.Author = item.Author;
+                video.Url = item.Url;
+                byte[] imageBytes = new WebClient().DownloadData(item.Thumbnail);
+                using(MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    video.Thumbnail = Image.FromStream(ms);
+                }
+                list.Add(video);
+                
+
+            }
+
+            videoBindingSource.DataSource = list;
+
+        }
+    }
+}
